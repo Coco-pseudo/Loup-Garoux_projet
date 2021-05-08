81cc3f91caedf63dfae02bbcef2d495ee205e8f1
MelyMelo8 <amelie.guedes9@gmail.com>
2021-05-08 19:25:41 +0200

    début def paquet

--- init.c
+++ init.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <math.h>
 
+#define NB 23
+#define TRUE 1
+#define FALSE 0
+
 // implanter des cartes d'extensions? le best of?( meilleures cartes choisies par la commu )
 enum cartes {
     AUCUN,
@@ -12,11 +16,54 @@ enum cartes {
     CUPIDON,
     PETITEFILLE,
     CHASSEUR,
+    VOLEUR, // tu l'avais pas mis, tu veux le retirer ?
 };
 
 typedef struct Stjoueur
 {
     char[10] nom; /*nom / pseudo*/
     int role;
+    int capitaine; /* oui = 1, non = 0 */
     int status; /*mort ou vivant, mort = 0, vivant = 1 */
 }Joueur;
+
+typedef struct Carte {
+    int carte; // sera le role du joueur, dépend de l'énumération
+    int dispo; // 1 : dans le paquet, 0 : dans la main d'un joueur
+} Carte;
+
+Carte* initPaquet(){ // TODO améliorer la fonction pour que le paquet dépende du nombre de joueur ... 
+    Carte* paquet = malloc(NB*sizeof(Carte));
+    int i = 0;
+    while (i != 4){
+        paquet[i].carte = LOUPGAROUS;
+        i++;
+    }
+    while (i != 17){ // 13 villageois
+        paquet[i].carte = VILLAGEOIS;
+        i++;
+    }
+    paquet[17].carte = VOYANTE;
+    paquet[18].carte = CUPIDON;
+    paquet[19].carte = PETITEFILLE;
+    paquet[20].carte = CHASSEUR;
+    paquet[21].carte = VOLEUR;
+    paquet[22].carte = SORCIERE;
+    for (int j=0; j<NB; j++){
+        paquet[i].dispo = TRUE;
+    }
+    return paquet;
+}
+
+int tireCarte(Carte* paquet){
+    int choix = AUCUN, position = -1;
+    while (position == -1){
+        position = rand()%NB;
+        if (paquet[position].dispo){
+            choix = paquet[position].carte;
+        } else {
+            position = -1; // carte non disponible => on repioche
+        }
+    }
+    return choix;
+}
