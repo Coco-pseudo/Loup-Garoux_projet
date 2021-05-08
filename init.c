881c45a44e13ef8837ebc16d5509036aa5c5d3b9
MelyMelo8 <amelie.guedes9@gmail.com>
2021-05-08 19:46:26 +0200

    organisation propre du code avec les headers (.h)

--- init.c
+++ init.c
@@ -1,37 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
 #include "init.h"
-
-#define NB 23
-#define TRUE 1
-#define FALSE 0
-
-// implanter des cartes d'extensions? le best of?( meilleures cartes choisies par la commu )
-enum cartes {
-    AUCUN,
-    SORCIERE,
-    LOUPGAROUS,
-    VOYANTE,
-    VILLAGEOIS,
-    CUPIDON,
-    PETITEFILLE,
-    CHASSEUR,
-    VOLEUR, 
-};
-
-typedef struct Stjoueur
-{
-    char[10] nom; /*nom / pseudo*/
-    int role;
-    int capitaine; /* oui = 1, non = 0 */
-    int status; /*mort ou vivant, mort = 0, vivant = 1 */
-} Joueur;
-
-typedef struct Carte {
-    int carte; // sera le role du joueur, dépend de l'énumération
-    int dispo; // 1 : dans le paquet, 0 : dans la main d'un joueur
-} Carte;
 
 Carte* initPaquet(){ // TODO améliorer la fonction pour que le paquet dépende du nombre de joueur ... 
     Carte* paquet = malloc(NB*sizeof(Carte));
---
+++ init.h
@@ -0,0 +1,24 @@
+#include "index.h"
+
+#ifndef INIT
+#define INIT
+
+    /*!
+    * \fn Carte* initPaquet()
+    * \brief initialise et retourne le paquet de cartes, soit l'ensemble des cartes que les joueurs utiliserons
+    */
+    Carte* initPaquet(); 
+
+    /*!
+    * \fn int tireCarte(Carte* paquet)
+    * \brief tire aléatoirement une carte du paquet, vérifie si elle est disponible, si oui, la rend indisponible et la retourne, sinon repioche jusqu'à retourner une carte disponible
+    */
+    int tireCarte(Carte* paquet);
+
+    /*!
+    * \fn void distribueCarte(Joueur* joueurs, int nb_joueur)
+    * \brief initialise la caractéristique role de chaque joueur en tirant à chacun leur tour une carte du paquet
+    */
+    void distribueCarte(Joueur* joueurs, int nb_joueur);
+    
+#endif 
